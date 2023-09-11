8455c87925f81a41282f470e69fa9ecfde38367c
neovenise <rodriguezdamien1677@gmail.com>
2023-09-11 11:32:32 +0200

    add sql script to initialize database, tables and datas example

---
+++ script.sql
@@ -0,0 +1,34 @@
+-- Création de la table pour le travail de révision
+-- De Damien RODRIGUEZ
+
+
+CREATE DATABASE IF NOT EXISTS db_etudiants;
+USE db_etudiants;
+
+DROP TABLE IF EXISTS etudiant;
+DROP TABLE IF EXISTS section;
+
+
+CREATE TABLE IF NOT EXISTS SECTION (
+    `idSection` INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `libelleSection` varchar(50) DEFAULT NULL
+    );
+
+    INSERT INTO SECTION(`libelleSection`) VALUES('BTS SIO 1 SLAM'),('BTS SIO 1 SISR'),('TERMINAL');
+
+CREATE TABLE IF NOT EXISTS ETUDIANT (
+    `idEtudiant` INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `nom` VARCHAR(25) NOT NULL,
+    `prenom` VARCHAR(20) NOT NULL,
+    `datenaissance` date default null,
+    `email` varchar(100) default null,
+    `telmobile` varchar(20) default null,
+    `idSection` integer default null
+    );
+
+ALTER TABLE ETUDIANT ADD CONSTRAINT `fk_etudiant_idsection` FOREIGN KEY (`idSection`) REFERENCES section(`idSection`);
+
+INSERT INTO `ETUDIANT`(`nom`,`prenom`,`datenaissance`,`email`,`telmobile`,`idSection`) VALUES
+('rodriguez','damien','2002-01-03','damien@damienrodriguez.fr','+33699887766',1),
+('Morabet','Moumen','2003-01-01','moumen@jsp.fr','+33 33 33 33 33',1),
+('Youssef','En nour','2099-99-99','yous@sef.en','+33 4 54 32 65 05',2)
