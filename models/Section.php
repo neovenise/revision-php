9282cd641337014c7ae9afa3452d3c7c2d19626d
neovenise <rodriguezdamien1677@gmail.com>
2023-09-15 08:42:02 +0200

    add section class file

---
+++ models/Section.php
@@ -0,0 +1,16 @@
+<?php
+class Section
+{
+
+    private int $idSection;
+    private string $nomSection;
+
+    public function __construct(int $id, string $name)
+    {
+        if (isset($id) && !isempty($id)) {
+
+        }
+    }
+
+
+}
