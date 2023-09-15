c6b51bc0212bc73c9a4bb2a31b13803fc0dfc997
neovenise <rodriguezdamien1677@gmail.com>
2023-09-15 08:41:38 +0200

    add student class and manager file

---
+++ models/Etudiant.php
@@ -0,0 +1,11 @@
+<?php
+require('EtudiantManager.php');
+require('Section.php')
+
+class Etudiant{
+
+    private int $idEtudiant;
+    private string $nom,$prenom,$email;
+    private Section $laSection;
+
+}
---
+++ models/EtudiantManager.php
@@ -0,0 +1,10 @@
+<?php
+require('DbManager.php');
+
+class EtudiantManager
+{
+
+
+
+
+}
