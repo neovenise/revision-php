417b3cfecc8894f0f10894a11a21114d6c73bef9
neovenise <rodriguezdamien1677@gmail.com>
2023-09-18 14:52:30 +0200

    complete Etudiant class and Section.php

--- models/Etudiant.php
+++ models/Etudiant.php
@@ -1,11 +1,55 @@
 <?php
 require('EtudiantManager.php');
-require('Section.php')
+require('Section.php');
 
-class Etudiant{
+class Etudiant
+{
 
     private int $idEtudiant;
-    private string $nom,$prenom,$email;
+    private string $nom, $prenom, $email, $telmobile;
+    private DateTime $datenaissance;
     private Section $laSection;
+
+    public function __construct(
+        int $id,
+        string $surname = 'à définir',
+        string $name = 'à définir',
+        string $mail = 'à définir',
+        string $phone = 'à définir'
+    ) {
+        if (!isset($id)) {
+            throw new Exception("l'ID de l'étudiant n'a pas été donné.");
+        }
+        $this->idEtudiant = $id;
+        $this->nom = $surname;
+        $this->prenom = $name;
+        $this->email = $mail;
+        $this->telmobile = $phone;
+    }
+
+    public function GetNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function GetPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function GetMail(): string
+    {
+        return $this->email;
+    }
+
+    public function GetID(): int
+    {
+        return $this->idEtudiant;
+    }
+
+    public function GetDateNaissance(): DateTime
+    {
+        return $this->datenaissance;
+    }
 
 }
--- models/Section.php
+++ models/Section.php
@@ -7,10 +7,28 @@ class Section
 
     public function __construct(int $id, string $name)
     {
-        if (isset($id) && !isempty($id)) {
-
+        if (isset($id) && !empty($id)) {
+            $this->idSection = $id;
+        } else {
+            throw new Exception("l'ID ne peut pas être vide");
+        }
+        if (isset($name) && !empty($name)) {
+            $this->nomSection = $name;
+        } else {
+            throw new Exception("le nom ne peut pas être vide");
         }
     }
+
+    public function GetNom(): string
+    {
+        return $this->nomSection;
+    }
+
+    public function GetId(): int
+    {
+        return $this->idSection;
+    }
+
 
 
 }
